aa5f523cda033767b9b2fe98c48f87e1b3852cc0
Ian Kuznicki <iankuz25@gmail.com>
2022-11-20 16:23:41 -0300

    Animation implementations

--- Assets/characterMovement.cs
+++ Assets/characterMovement.cs
@@ -5,42 +5,64 @@ using UnityEngine;
 
 public class characterMovement : MonoBehaviour
 {
-    [SerializeField] float jumpForce;
+    [SerializeField] private Animator anim;
+    [SerializeField] private float rightSpeed;
+    [SerializeField] private float jumpForce;
     private bool jump = false;
 
     private Rigidbody2D rb;
     private Collider2D cl;
+
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-     // cl = GetComponent<Collider2D>();
+        // cl = GetComponent<Collider2D>();
 
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+
+        Slide();
         Jump();
+        MoveToRight();
     }
 
-    void Jump()
+    private void Slide()
     {
 
-        if (Input.GetKeyDown(KeyCode.Space)&& !jump)
-        {
-            Debug.Log("Salto");
-            rb.AddForce(new Vector2(0,jumpForce));
-            jump = true;
+    }
 
+    private void MoveToRight()
+    {
+        transform.Translate(Vector2.right * rightSpeed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && !jump)
+        {
+            if (Random.Range(0, 2) == 1)
+                anim.SetTrigger("Jump");
+            else
+                anim.SetTrigger("Mortal");
+
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            jump = true;
         }
     }
-   
-    void OnCollisionEnter2D(Collision2D other)
+
+
+    private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.name == "ground")
         {
-            Debug.Log("Entro"); 
+            anim.SetTrigger("Run");
+            Debug.Log("Entro");
             jump = false;
         }
     }
