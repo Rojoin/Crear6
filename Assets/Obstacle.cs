f893fc696f083610a056d85d03b43bf104d664ff
Rojoin <nachoarrastua@gmail.com>
2022-11-20 16:00:56 -0300

    Add Obsstacle

---
+++ Assets/Obstacle.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private BoxCollider2D bc;
+
+
+    [SerializeField] private int damageDone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bc = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<characterMovement>().Damage(damageDone);
+        }
+    }
+
+}
